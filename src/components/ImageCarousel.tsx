885f6f8750498dd4b9cbe9310aac612708cf5d73
Omry-Ann <omry@ann-education.com>
2024-12-30 22:49:07 +0200

    images roll fix

--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -36,7 +36,7 @@ const ImageCarousel: React.FC = () => {
     const container = containerRef.current;
 
     if (container) {
-      const singleImageWidth = 200; // Image width in pixels
+      const singleImageWidth = 220; // Image width in pixels
       const gap = 50; // Gap between images
       const numImages = images.length;
       const totalWidth = (singleImageWidth + gap) * numImages;
