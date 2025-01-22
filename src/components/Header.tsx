c8adb59624f41080fc7540c60a231e51d698c5dc
Omry-Ann <omry@ann-education.com>
2025-01-22 23:02:08 +0200

    heb

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ import { DeviceContext } from "../App";
 
 const Header: React.FC = () => {
 
-  const { isMobile } = useContext(DeviceContext);
+  const { isMobile, lan } = useContext(DeviceContext);
   useEffect(() => {
     gsap.fromTo(
       [".header-title", ".mobile-header-title"],
@@ -20,19 +20,44 @@ const Header: React.FC = () => {
       <img src={logo}></img>
       {!isMobile && (
         <div className="header-title">
-          <p>Nestled within the vibrant Jaffa Port, </p> 
-          <p>Café Blue is a cozy spot for locals and visitors. With specialty</p>
-          <p>and delicious pastries, every detail is crafted to perfection.</p>
-          <p>Oh... and the coffee? Simply exceptional!</p>
+          {!lan && (
+            <>
+              <p>Nestled within the vibrant Jaffa Port, </p> 
+              <p>Café Blue is a cozy spot for locals and visitors. With specialty</p>
+              <p>and delicious pastries, every detail is crafted to perfection.</p>
+              <p>Oh... and the coffee? Simply exceptional!</p>
+            </>
+          )}
+          {lan && (
+            <>
+              <p>,ליד הסמטאות הציוריות של נמל יפו</p> 
+              <p>.קפה בלו מביא איתו אווירה מיוחדת לתושבי יפו ולמבקרים</p>
+              <p>.עם קפה מיוחד ומאפים טעימים במיוחד, כל פרט קטן חשוב לנו</p>
+              <p>!אם אתם מחפשים מקום להירגע על קפה מצוין - אתם במקום הנכון</p>
+            </>
+          )}
         </div>
       )}
       {isMobile && (
         <div className="mobile-header-title">
-          <p>Nestled within the vibrant Jaffa Port, </p> 
-          <p>Café Blue is a cozy spot for locals and visitors.</p>
-          <p>With specialty and delicious pastries, every detail</p>
-          <p>is crafted to perfection. Oh... and the coffee?</p>
-          <p>Simply exceptional!</p>
+          {!lan && (
+            <>
+              <p>Nestled within the vibrant Jaffa Port, </p> 
+              <p>Café Blue is a cozy spot for locals and visitors.</p>
+              <p>With specialty and delicious pastries, every detail</p>
+              <p>is crafted to perfection. Oh... and the coffee?</p>
+              <p>Simply exceptional!</p>
+            </>
+          )}
+          {lan && (
+            <>
+              <p>,ליד הסמטאות הציוריות של נמל יפו</p> 
+              <p>קפה בלו מביא איתו אווירה מיוחדת לתושבי יפו </p>
+              <p>,ולמבקרים. עם קפה מיוחד ומאפים טעימים במיוחד</p>
+              <p>כל פרט קטן חשוב לנו. אם אתם מחפשים מקום להירגע</p>
+              <p>!על קפה מצוין - אתם במקום הנכון</p>
+            </>
+          )}
         </div>
       )}
     </header>
