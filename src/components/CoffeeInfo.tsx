2f91b14bbba0f73099c2534758f492f60ec704dc
Omry-Ann <omry@ann-education.com>
2025-02-11 23:56:49 +0200

    accessability

--- src/components/CoffeeInfo.tsx
+++ src/components/CoffeeInfo.tsx
@@ -1,44 +1,44 @@
-import React, { useEffect } from "react";
-import gsap from "gsap";
-import "./CoffeeInfo.scss";
+// import React, { useEffect } from "react";
+// import gsap from "gsap";
+// import "./CoffeeInfo.scss";
 
-const CoffeeInfo: React.FC = () => {
-  useEffect(() => {
-    gsap.fromTo(
-      "coffeeInfo",
-      { opacity: 0, x: 100 },
-      { opacity: 1, x: 0, duration: 1, scrollTrigger: "coffeeInfo" }
-    );
-  }, []);
+// const CoffeeInfo: React.FC = () => {
+//   useEffect(() => {
+//     gsap.fromTo(
+//       "coffeeInfo",
+//       { opacity: 0, x: 100 },
+//       { opacity: 1, x: 0, duration: 1, scrollTrigger: "coffeeInfo" }
+//     );
+//   }, []);
 
-  return (
-    <section className={"coffeeInfo"}>
-      <h2>Coffee for Home</h2>
-      <ul>
-        <li>House Blend - 200g/400g: 40/75 ILS</li>
-        <li>800g/1200g: 135/200 ILS</li>
-      </ul>
-      <h2>Collaborations</h2>
-      <h4>Chupchik</h4>
-      <p> Tzofzik is a roastery and coffee cart located in Beit Yehoshua. It began its journey </p>
-      <p> in late 2018 when owner Or Ziman transitioned from the Tel Aviv café and restaurant</p>
-      <p> scene back to Beit Yehoshua, where he was born and raised. Or's desire to introduce </p>
-      <p> local residents to the world of coffee started with a coffee cart, the first in the </p>
-      <p> area to serve locally roasted coffee. Over time, Or delved deeper into coffee and roasting, </p>
-      <p> winning competitions like the "Best Coffee" competition in 2024, leading workshops, and </p>
-      <p> participating in local and international initiatives. What began as a small roastery </p>
-      <p> in a family farm room has grown into a significant player in the Israeli coffee scene, </p>
-      <p> supplying fresh coffee to hundreds of private and business clients. At Tzofzik, Or and </p>
-      <p> his team emphasize quality, from raw materials to packaging, while maintaining accessibility </p>
-      <p> and fostering a down-to-earth approach.</p>
-      <h4>Bloom's Coffee</h4>
-      <p>A specialty coffee roastery founded by Assaf Hania, who caught the specialty coffee bug </p>
-      <p>in Melbourne, Australia. Since then, his passion for bringing specialty coffee to a wider </p>
-      <p>audience has only grown. After returning to Israel, Assaf opened a coffee cart in Pardes </p>
-      <p>Hanna-Karkur and, shortly after, established a nearby roastery. The roastery continues to grow </p>
-      <p>and gain widespread recognition, thanks to Assaf's personal touch and the uniqueness of the coffee.</p>
-    </section>
-  );
-};
+//   return (
+//     <section className={"coffeeInfo"}>
+//       <h2>Coffee for Home</h2>
+//       <ul>
+//         <li>House Blend - 200g/400g: 40/75 ILS</li>
+//         <li>800g/1200g: 135/200 ILS</li>
+//       </ul>
+//       <h2>Collaborations</h2>
+//       <h4>Chupchik</h4>
+//       <p> Tzofzik is a roastery and coffee cart located in Beit Yehoshua. It began its journey </p>
+//       <p> in late 2018 when owner Or Ziman transitioned from the Tel Aviv café and restaurant</p>
+//       <p> scene back to Beit Yehoshua, where he was born and raised. Or's desire to introduce </p>
+//       <p> local residents to the world of coffee started with a coffee cart, the first in the </p>
+//       <p> area to serve locally roasted coffee. Over time, Or delved deeper into coffee and roasting, </p>
+//       <p> winning competitions like the "Best Coffee" competition in 2024, leading workshops, and </p>
+//       <p> participating in local and international initiatives. What began as a small roastery </p>
+//       <p> in a family farm room has grown into a significant player in the Israeli coffee scene, </p>
+//       <p> supplying fresh coffee to hundreds of private and business clients. At Tzofzik, Or and </p>
+//       <p> his team emphasize quality, from raw materials to packaging, while maintaining accessibility </p>
+//       <p> and fostering a down-to-earth approach.</p>
+//       <h4>Bloom's Coffee</h4>
+//       <p>A specialty coffee roastery founded by Assaf Hania, who caught the specialty coffee bug </p>
+//       <p>in Melbourne, Australia. Since then, his passion for bringing specialty coffee to a wider </p>
+//       <p>audience has only grown. After returning to Israel, Assaf opened a coffee cart in Pardes </p>
+//       <p>Hanna-Karkur and, shortly after, established a nearby roastery. The roastery continues to grow </p>
+//       <p>and gain widespread recognition, thanks to Assaf's personal touch and the uniqueness of the coffee.</p>
+//     </section>
+//   );
+// };
 
-export default CoffeeInfo;
+// export default CoffeeInfo;
