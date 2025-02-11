cc5d31b7b529bb934d68337d4a106956d9c02a74
Omry-Ann <omry@ann-education.com>
2025-02-11 23:57:56 +0200

    accessability

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState, createContext, useContext } from 'react';
 import Header from "./components/Header";
 import AboutUs from "./components/AboutUs";
-import Renovation from "./components/Renovation";
-import CoffeeInfo from "./components/CoffeeInfo";
-import Footer from "./components/Footer";
+// import Renovation from "./components/Renovation";
+// import CoffeeInfo from "./components/CoffeeInfo";
+// import Footer from "./components/Footer";
 import ImageCarousel from "./components/ImageCarousel";
 import MoreInfo from "./components/MoreInfo";
 import Menu from "./components/Menu";
